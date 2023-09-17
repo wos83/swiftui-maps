b7ea5df47f2d4a5a81b990d2447041356f314bd2
Willian Santos <m16@m16.com.br>
2023-09-17 08:43:42 -0300

    Initial Commit

---
+++ maps-v1a/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  maps-v1a
+//
+//  Created by Willian Santos on 9/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ maps-v1a/maps_v1aApp.swift
@@ -0,0 +1,17 @@
+//
+//  maps_v1aApp.swift
+//  maps-v1a
+//
+//  Created by Willian Santos on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct maps_v1aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
