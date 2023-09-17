b8628bd773b7a41ab1d1ba7d376c7396629ef711
Willian Santos <m16@m16.com.br>
2023-09-17 14:15:00 -0300

    WOS Maps (SwiftUI iOS 17)

---
+++ maps-v1a/LocationDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailsView.swift
+//  maps-v1a
+//
+//  Created by Willian Santos on 9/17/23.
+//
+
+import SwiftUI
+
+struct LocationDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailsView()
+}
